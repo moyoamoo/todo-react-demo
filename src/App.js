1009dbcd2eb36a0e9a066f7cbff367721f9466ac
Moyo A-P <moyo.amoo@hotmail.com>
2024-01-26 11:43:03 +0000

    add todos

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { todos: ["Clean car", "Buy Bread", "Feed fish"] };
+
+  onTodoInput = (e) => {
+    this.setState({ todoInput: e.target.value });
+  };
+
+ onAddClick = ()=>{
+  const todos = [...this.state.todos];
+  todos.push(this.state.todoInput)
+  this.setState({todos})
+ }
+
+  render() {
+    console.log(this.state);
+    const { todos } = this.state;
+
+    return (
+      <>
+        <input type="text" onInput={this.onTodoInput} />
+        <button onClick={this.onAddClick}>Add</button>
+        {todos.map((todo) => {
+          return <p>{todo}</p>;
+        })}
+      </>
+    );
+  }
+}
+
+export default App;
